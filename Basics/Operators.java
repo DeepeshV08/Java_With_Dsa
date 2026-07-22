f18432181f3a26faf19d6e7e93a7f4aa3122f89b
Your Name <deepesh3950lodhi@gmail.com>
2026-07-23 04:19:06 +0530

    Operaors In Java

---
+++ Basics/Operators.java
@@ -0,0 +1,71 @@
+public class Operators{
+    public static void main(String[] args) {
+        
+        /**
+         * Operators is a symbol that perform an operationon one or more operands
+         * 
+         * There are various types of operator
+         * 1. Arithmetic operator
+         *  + , - , * , / . %
+         * 
+         * 2. Relational Operator
+         * == , < , <= ,> , >= , !=
+         * 
+         * 3. Logical operator - only apply on Conditions or true or false value
+         * && , || , !
+         * 
+         * 4. Shorthand operator
+         * = , += , -= , *= , /= , %=
+         */
+
+        /*Assingment operator
+        int solvedThisWeek = 25;
+        int solvedLastWeek = 15;
+
+        int total = solvedThisWeek + solvedLastWeek;
+      int difference = solvedThisWeek - solvedLastWeek;
+        int projected = solvedThisWeek * 4;
+        int average = solvedThisWeek / 7;
+        int remainder = solvedThisWeek % 7;
+
+        System.out.println(total);
+        System.out.println(difference);
+        System.out.println(projected);
+        System.out.println(average);
+        System.out.println(remainder);
+        */
+
+
+        /* Relational Operators
+        int currentStreak = 45;
+        int targetStreak = 50;
+
+        System.out.println(currentStreak == targetStreak);
+        System.out.println(currentStreak != targetStreak);
+        System.out.println(currentStreak > targetStreak);
+        System.out.println(currentStreak < targetStreak);
+        System.out.println(currentStreak >= targetStreak);
+        System.out.println(currentStreak <= targetStreak);
+         */
+
+         /* Logical Operators
+        boolean completedDSA = true;
+        boolean completedCore = false;
+
+        System.out.println(completedDSA && completedCore);
+        System.out.println(completedDSA || completedCore);
+        System.out.println(!completedCore);
+        */
+
+        /* Unary Operators
+        int activeUsers = 100;
+
+        int prefix = ++activeUsers;
+        int postfix = activeUsers++;
+
+        System.out.println(prefix);
+        System.out.println(postfix);
+        System.out.println(activeUsers);
+        */
+    }
+}
