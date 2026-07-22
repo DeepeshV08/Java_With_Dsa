de5578571f2a7f7f7ab1cfeadff1a8b0e7f57364
Your Name <deepesh3950lodhi@gmail.com>
2026-07-22 18:51:38 +0530

    Data types and variable

---
+++ Basics/DataTypes.java
@@ -0,0 +1,33 @@
+public class DataTypes {
+    
+    public static void main(String[] args) {
+        
+        // datatype is defined which value a variable can store
+
+        // There are two types of data types
+        // Primtive Data Types
+        // Non- Primitive Data Types
+
+        // 1. Primitive - Integer
+
+        byte b = 100; 
+        short s = 2000;
+        int i = 233232432;
+        long l = 8482482428928l;
+
+        //Floating point 
+        float f = 12.445f;
+        double d = 8282.8484;
+
+        // Non Numeric data types
+        char ch = 'A';
+        boolean val = true;
+
+
+
+        // 2. Non Primitive Data types
+        String name = "Deepesh";
+        int[] arr = new int[5];
+        
+    }
+}
---
+++ Basics/Variable.java
@@ -0,0 +1,16 @@
+public class Variable {
+    
+    public static void main(String[] args) {
+        
+        // Variable is a container used to store value
+        String name = "Deepesh";
+        System.out.println(name);
+
+        int age; // declaration
+        age = 20;//intialization
+
+        int a = 10; //declaration and initialization in same line
+
+        
+    }
+}
