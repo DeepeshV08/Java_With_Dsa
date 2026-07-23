e186e1ffc97fb4e22da59d0db2dcfb7e8435c3ae
Your Name <deepesh3950lodhi@gmail.com>
2026-07-23 05:36:25 +0530

    Conditionals

---
+++ Basics/Conditionals.java
@@ -0,0 +1,79 @@
+public class Conditionals {
+    
+    public static void main(String[] args) {
+        
+        /**
+         * IF-ELSE
+         * if(true){
+         * this code execute
+         * }else{
+         * this code execute
+         * }
+         * 
+         * IF-ELSE ladder
+         */
+
+        //  int age = 19;
+        //  if(age>= 18){
+        //     System.out.println("Your are eligible to vote");
+        //  }else{
+        //     System.out.println("Your are not eligible");
+        //  }
+
+        //If else ladder
+        // int a = 5;
+        // if(a == 1){
+        //     System.out.println("Matched");
+        // }else if(a == 2){
+        //     System.out.println("Matched");
+        // }else if(a == 3){
+        //     System.out.println("Matched");
+        // }else if(a == 4){
+        //     System.out.println("Matched");
+        // }else if(a == 5){
+        //     System.out.println("Matched");
+        // }else{
+        //     System.out.println("Not Matched");
+        // }
+
+        //Nested IF - ELSE
+        // int age = 20;
+        // char gender = 'M';
+        // if(gender == 'M'){
+        //     if(age >= 18){
+        //         System.out.println("You are male and age is > 18");
+        //     }else{
+        //         System.out.println("You are male but have age < 18");
+        //     }
+        // }else{
+        //     if(age>18){
+        //         System.out.println("you are female having age > 18");
+        //     }else{
+        //         System.out.println("you are female having age < 18");
+
+        //     }
+        // }
+
+        /*
+         * Switch Case
+         */
+        // int a = 7;
+        // switch (a) {
+        //     case 1 -> System.out.println("Monday");
+        //     case 2 -> System.out.println("Tuesday");
+        //     case 3 -> System.out.println("Wednesday");
+        //     case 4 -> System.out.println("Thurday");
+        //     case 5 -> System.out.println("Friday");
+        //     case 6 -> System.out.println("Saturday");
+        //     case 7 -> System.out.println("Sunday");
+        // }
+
+        // Ternary Operators
+        // String day = "Yes";
+        // System.out.println(day == "Yes" ? "Day" : "Night");
+
+        // int age = 100;
+        // age = (age == 100) ? 25 : 50;
+        // System.out.println(age);
+    }
+}
