d0f3caa80c0e11f1213588ec8a6135626c827b92
Your Name <deepesh3950lodhi@gmail.com>
2026-07-24 04:58:20 +0530

    Loops In java

---
+++ Basics/Loops.java
@@ -0,0 +1,58 @@
+public class Loops {
+    
+    public static void main(String[] args) {
+
+        // for(initialization ; condition ; updation){
+        // code
+        // }
+
+        // for (int i = 1; i <= 5; i++) {
+        //     System.out.println(i);
+        // }
+
+        // for(int i = 1 ; i<= 10 ;i++){
+        //     System.out.println("Hemloooo");
+        // }
+
+        // odd number
+        // for(int i = 1 ; i<= 50 ; i+=2){
+        //     System.out.println(i);
+        // }
+
+        // Even number
+        // for(int i = 2 ; i<= 50 ; i++){
+        //     System.out.println(i);
+        // }
+
+        // Nested Loop
+
+        // for(int i = 1 ; i<= 3 ; i++){
+        //     for(int j = 1 ; j<= 3 ; j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i = 1 ; i<= 3 ; i++){
+        //     for(int j = 1 ; j<= 3; j++){
+        //         System.out.print("i = " + i +", j = "+j+" ");
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i = 1; i<= 5 ; i++){
+        //     if(i == 2){
+        //         continue;
+        //     }
+        //     System.out.println(i);
+        //     break;
+        // }
+
+        // Do while loop
+        int i = 11;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i <= 10);
+    }
+}
