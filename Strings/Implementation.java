64bb033353f9874b6db2d88b6a879349840441cc
Your Name <deepesh3950lodhi@gmail.com>
2026-07-26 05:12:33 +0530

    String In Java

---
+++ Strings/Implementation.java
@@ -0,0 +1,68 @@
+
+
+public class Implementation {
+    
+    public static void main(String[] args) {
+        
+        // String Literals
+        // String str = "Deepesh";
+
+        // String Objects
+        // String st = new String("Deepesh");
+
+
+
+        // True or false;
+        // String name1 = "Deepesh";
+        // String name2 = "Deepesh";
+        // System.out.println(name1 == name2);
+
+        // String str1 = new String("Deepesh");
+        // String str2 = new String("Deepesh");
+        // System.out.println(str1 == str2);
+        // System.out.println(str1.equals(str2));
+        // System.out.println(str1.compareTo(str2));
+
+
+
+        // String name ="Deepesh";
+        // System.out.println(name);
+        
+
+
+        // Methods oF String
+
+        // String str = "Deepesh";
+        // System.out.println(str.length());
+        // System.out.println(str.indexOf('p'));
+        // System.out.println(str.charAt(5));
+        // System.out.println(str.toLowerCase());
+        // System.out.println(str.toUpperCase());
+        // System.out.println(str.isEmpty());
+
+
+        // toCharArray
+        // String name = "Deepesh";
+        // char [] crr = name.toCharArray();
+        // for(char  ch : crr){
+        //     System.out.print(ch + " -> ");
+        // }
+
+        //replace
+        // String name = "Deepesh";
+        // System.out.println(name.replace('e','z'));
+        // System.out.println(name);
+
+        // Split
+        // String name= "You are my love";
+        // String [] str = name.split(" ");
+        // System.out.println(Arrays.toString(str));
+
+        //startWith and endWith
+        // String name = "Deepesh";
+        // System.out.println(name.startsWith("Dee"));
+        // System.out.println(name.endsWith("sh"));
+
+    }
+    
+}
